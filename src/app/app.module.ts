c7fcf3a26765a452ac8fdd3cbea203abc1d55120
josegonta97 <josegonta97@gmail.com>
2023-06-07 16:09:27 +0200

    [DEN-2] Pantalla gira al iniciar la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
+
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private screenOrientation: ScreenOrientation) {
+    // get current
+    console.log(this.screenOrientation.type); // logs the current orientation, example: 'landscape'
+
+    // set to landscape
+    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
+  }
 
   ngOnInit() {
   }
