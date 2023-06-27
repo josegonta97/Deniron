9dc8852f7e83e9dae11951c03d265e0e1b6236ec
josegonta97 <130555641+ualjgt479@users.noreply.github.com>
2023-06-27 19:57:08 +0200

    muchos cambios

--- src/app/pages/ciudad/ciudad.page.ts
+++ src/app/pages/ciudad/ciudad.page.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ciudad.page.scss'],
 })
 export class CiudadPage implements OnInit {
+  
+  
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
+    const myCustomIcon = "../../../assets/img/maderasrc.png";
   }
 
 }
